85857c71c13fa7a340ee6901098dde258658839a
bwang1008 <bwang1008@gmail.com>
2025-03-03 00:24:51 -0600

    Namespace prefix in multiplication test

--- BigInt/tests/test_multiplication.cpp
+++ BigInt/tests/test_multiplication.cpp
@@ -5,11 +5,13 @@
 
 #include "BigInt/include/big_int.hpp"
 
-TEST_CASE("Multiplication with 0", "[mul]") {
-    BigInt x = 0_b;
-    BigInt y = 12345678901234567890_b;
+using BigInt::operator""_b;
 
-    BigInt z = x * y;
+TEST_CASE("Multiplication with 0", "[mul]") {
+    BigInt::BigInt x = 0_b;
+    BigInt::BigInt y = 12345678901234567890_b;
+
+    BigInt::BigInt z = x * y;
     REQUIRE(z.str() == "0");
 }
 
@@ -17,59 +19,59 @@ TEST_CASE("Multiplication of lots of positives", "[mul]") {
     auto i = GENERATE(range(0, 11));
     auto j = GENERATE(range(0, 11));
 
-    BigInt x(i);
-    BigInt y(j);
-    BigInt z = x * y;
+    BigInt::BigInt x(i);
+    BigInt::BigInt y(j);
+    BigInt::BigInt z = x * y;
 
     REQUIRE(z.str() == std::to_string(i * j));
 }
 
 TEST_CASE("Multiplication of medium-sized positives", "[mul]") {
-    BigInt x = 12345678901234567890_b;
-    BigInt y = 98765432109876543210_b;
-    BigInt z = x * y;
+    BigInt::BigInt x = 12345678901234567890_b;
+    BigInt::BigInt y = 98765432109876543210_b;
+    BigInt::BigInt z = x * y;
 
     REQUIRE(z.str() == "1219326311370217952237463801111263526900");
 }
 
 TEST_CASE("Multiplication with negative 1", "[mul]") {
-    BigInt x = 123_b;
-    BigInt y = -1_b;
-    BigInt z = x * y;
+    BigInt::BigInt x = 123_b;
+    BigInt::BigInt y = -1_b;
+    BigInt::BigInt z = x * y;
 
     REQUIRE(z.str() == "-123");
 }
 
 TEST_CASE("Multiplication with 1", "[mul]") {
-    BigInt x = -123_b;
-    BigInt y = 1_b;
-    BigInt z = x * y;
+    BigInt::BigInt x = -123_b;
+    BigInt::BigInt y = 1_b;
+    BigInt::BigInt z = x * y;
 
     REQUIRE(z.str() == "-123");
 }
 
 TEST_CASE("Multiplication of two negatives", "[mul]") {
-    BigInt x = -123_b;
-    BigInt y = -1_b;
-    BigInt z = x * y;
+    BigInt::BigInt x = -123_b;
+    BigInt::BigInt y = -1_b;
+    BigInt::BigInt z = x * y;
 
     REQUIRE(z.str() == "123");
 }
 
 TEST_CASE("Multiplication to square", "[mul]") {
-    BigInt x{"98765432109876543210"};
-    BigInt y{"98765432109876543210"};
+    BigInt::BigInt x{"98765432109876543210"};
+    BigInt::BigInt y{"98765432109876543210"};
 
-    BigInt z = x * y;
+    BigInt::BigInt z = x * y;
     REQUIRE(z.str() == "9754610579850632525677488187778997104100");
 }
 
 TEST_CASE("Multiplication of large positives", "[mul]") {
-    BigInt x{"31415926535897932384626433832795028841971693993751058209749445923"
+    BigInt::BigInt x{"31415926535897932384626433832795028841971693993751058209749445923"
              "078164062862089986280348253421170679"};
-    BigInt y{"27182818284590452353602874713526624977572470936999595749669676277"
+    BigInt::BigInt y{"27182818284590452353602874713526624977572470936999595749669676277"
              "240766303535945713821785251664274"};
-    BigInt z = x * y;
+    BigInt::BigInt z = x * y;
 
     REQUIRE(
         z.str() ==
