47ebb24c2d3a4be53b37ea6b65f139d3ef631ed8
bwang1008 <bwang1008@gmail.com>
2025-03-23 23:12:53 -0500

    Add division operator tests

--- BigInt/tests/arithmetic/division/test_division.cpp
+++ BigInt/tests/arithmetic/division/test_division.cpp
@@ -1,7 +1,10 @@
-#include <utility> // std::pair
+#include <stdexcept> // std::invalid_argument
+#include <utility>   // std::pair
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_range.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/matchers/catch_matchers_exception.hpp>
 
 #include "BigInt/include/big_int.hpp"
 
@@ -18,4 +21,39 @@ TEST_CASE("Division of lots of positives", "[div]") {
 
     REQUIRE(quotient == BigInt::BigInt(i / j));
     REQUIRE(remainder == BigInt::BigInt(i % j));
+}
+
+TEST_CASE("Division operator", "[div]") {
+    const BigInt::BigInt x(5);
+    const BigInt::BigInt y(2);
+    const BigInt::BigInt z = x / y;
+    REQUIRE(z == BigInt::BigInt(5 / 2));
+}
+
+TEST_CASE("Division positive with negative", "[div]") {
+    const BigInt::BigInt x(5);
+    const BigInt::BigInt y(-2);
+    const BigInt::BigInt z = x / y;
+    REQUIRE(z == BigInt::BigInt(5 / -2));
+}
+
+TEST_CASE("Division negative with positive", "[div]") {
+    const BigInt::BigInt x(-5);
+    const BigInt::BigInt y(2);
+    const BigInt::BigInt z = x / y;
+    REQUIRE(z == BigInt::BigInt(-5 / 2));
+}
+
+TEST_CASE("Division negative with negative", "[div]") {
+    const BigInt::BigInt x(-5);
+    const BigInt::BigInt y(-2);
+    const BigInt::BigInt z = x / y;
+    REQUIRE(z == BigInt::BigInt(-5 / -2));
+}
+
+TEST_CASE("Division by zero", "[div]") {
+    const BigInt::BigInt x(5);
+    const BigInt::BigInt y(0);
+    REQUIRE_THROWS_MATCHES(x / y, std::invalid_argument,
+                           Catch::Matchers::Message("Cannot divide by 0"));
 }
