f5095bb490baf156a5228ea8914c2afb1a443fd1
bwang1008 <bwang1008@gmail.com>
2025-06-04 17:54:43 -0500

    Move normalize_digits to be in constructor

--- BigInt/BigInt/src/constructor.cpp
+++ BigInt/BigInt/src/constructor.cpp
@@ -39,6 +39,23 @@ BigInt::BigInt(bool negative_, std::vector<int> digits_)
     this->normalize_digits();
 }
 
+auto BigInt::normalize_digits() -> void {
+    // remove leading 0's
+    while(!this->digits.empty() && this->digits.back() == 0) {
+        this->digits.pop_back();
+    }
+
+    // if no digits, initiailize with one 0
+    if(this->digits.empty()) {
+        this->digits.push_back(0);
+    }
+
+    // turn -0 to 0
+    if(this->digits.size() == 1 && this->digits[0] == 0) {
+        this->negative = false;
+    }
+}
+
 inline namespace literals {
 auto operator"" _b(const char *s) -> BigInt { return BigInt(s); }
 } // namespace literals
