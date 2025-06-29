922f6125158fd401d09d2df6259ab978d5c2cc59
bwang1008 <bwang1008@gmail.com>
2025-06-29 14:57:03 -0500

    More output tests

--- BigInt/Rational/tests/test_output.cpp
+++ BigInt/Rational/tests/test_output.cpp
@@ -3,16 +3,22 @@
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("output positive fraction", "[output]") {
-    const BigInt::Rational r(5, 3);
+    const BigInt::Rational r(10, 6);
+    REQUIRE(r.get_numerator() == BigInt::BigInt(5));
+    REQUIRE(r.get_denominator() == BigInt::BigInt(3));
     REQUIRE(r.str() == "5/3");
 }
 
 TEST_CASE("output negative fraction", "[output]") {
     const BigInt::Rational r(-3, 5);
+    REQUIRE(r.get_numerator() == BigInt::BigInt(-3));
+    REQUIRE(r.get_denominator() == BigInt::BigInt(5));
     REQUIRE(r.str() == "-3/5");
 }
 
 TEST_CASE("output zero fraction", "[output]") {
     const BigInt::Rational r;
+    REQUIRE(r.get_numerator() == BigInt::BigInt(0));
+    REQUIRE(r.get_denominator() == BigInt::BigInt(1));
     REQUIRE(r.str() == "0/1");
 }
