ba842830c697ecbc949edfacc08937def9380b98
bwang1008 <bwang1008@gmail.com>
2025-11-05 23:28:28 -0600

    Add medium test for toom-cook

--- BigInt/Multiplier/tests/test_toom_cook.cpp
+++ BigInt/Multiplier/tests/test_toom_cook.cpp
@@ -14,3 +14,15 @@ TEST_CASE("small test case", "[mul]") {
     const BigInt::BigInt product = multiplier.multiply(a2, b2);
     REQUIRE(product == BigInt::BigInt(a * b));
 }
+
+TEST_CASE("medium test case", "[mul]") {
+    BigInt::ToomCookMultiplier multiplier(3);
+    const int a = 2730; // 101010101010_2
+    const int b = 2205; // 100010011101_2
+
+    const BigInt::BigInt a2(a);
+    const BigInt::BigInt b2(b);
+
+    const BigInt::BigInt product = multiplier.multiply(a2, b2);
+    REQUIRE(product == BigInt::BigInt(a * b));
+}
