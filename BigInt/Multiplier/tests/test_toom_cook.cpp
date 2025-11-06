1089e22f47656514617235297d175921f9a17630
bwang1008 <bwang1008@gmail.com>
2025-11-06 00:28:30 -0600

    Add test with different sized inputs

--- BigInt/Multiplier/tests/test_toom_cook.cpp
+++ BigInt/Multiplier/tests/test_toom_cook.cpp
@@ -36,3 +36,15 @@ TEST_CASE("medium test case", "[mul]") {
     const BigInt::BigInt product = multiplier.multiply(a2, b2);
     REQUIRE(product == BigInt::BigInt(a * b));
 }
+
+TEST_CASE("different sizes", "[mul]") {
+    BigInt::ToomCookMultiplier multiplier(3);
+    const int a = 2730; // 101010101010_2
+    const int b = 13;   // 1101_2
+
+    const BigInt::BigInt a2(a);
+    const BigInt::BigInt b2(b);
+
+    const BigInt::BigInt product = multiplier.multiply(a2, b2);
+    REQUIRE(product == BigInt::BigInt(a * b));
+}
